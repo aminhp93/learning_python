217aefabc9114ee3a44ee4acd7b5d3420314e2e2
Minh Pham <minhpham@myhost.local>
2018-01-08 19:49:11 +0700

    34 - Add Timer and mindmeister

--- src/learning_python/urls.py
+++ src/learning_python/urls.py
@@ -1,31 +1,10 @@
-"""learning_python URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='Pastebin API')
-
 from .views import Home, Contact, RootAPIView, translate, autocomplete
-
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
@@ -40,33 +19,19 @@ urlpatterns = [
     url(r'^search/', include('haystack.urls')),
     url(r'^search/autocomplete/$', autocomplete),
 
-
     # =========================== API ====================================
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-refresh/', refresh_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
+
     url(r'^api/$', RootAPIView.as_view(), name='root-api'),
     url(r'^api/users/', include('accounts.api.urls', namespace='users-api')),
     url(r'^api/posts/', include('posts.api.urls', namespace='posts-api')),
     url(r'^api/comments/', include('comments.api.urls', namespace='comments-api')),  
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^swagger/$', schema_view), 
     url(r'^markdownx/', include('markdownx.urls')),
-
 ]
 
 urlpatterns += i18n_patterns(
     url(r'^translate/$', translate, name='translate'),
 )
-
-# Django Resframework Authentication
-
-
-
-handler400 = 'learning_python.views.error400'
-handler403 = 'learning_python.views.error403'
-handler404 = 'learning_python.views.error404'
-handler500 = 'learning_python.views.error500'
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- src/learning_python/views.py
+++ src/learning_python/views.py
@@ -9,34 +9,21 @@ from rest_framework.serializers import HyperlinkedIdentityField
 from rest_framework.views import exception_handler
 from rest_framework.views import APIView
 from rest_framework.reverse import reverse
-from rest_framework.exceptions import (
-		APIException, 
-		NotFound, 
-		ParseError, 
-		PermissionDenied
-	)
+
+from haystack.query import SearchQuerySet
 
 from posts.api.serializers import PostListSerializer
 from posts.models import Post
-
-from haystack.query import SearchQuerySet
 
 
 class Home(View):
 	def get(self, request, *args, **kwargs):
 		template = "home.html"
-		context = {"task_list": ["Create Book Model", 
-		"Crud Book", "Admin", "API Book Model",\
-		"account user: send mail", "post app (Viblo)",\
-		"project app (video content tutorial) ", "Regex",\
-		"Multi language", "Docker, pusher, notification",\
-		"Markdown - Comment", "Tag App"
-		]}
+		context = {'count_space': range(6)}
 		return render(request, template, context)
 
 class Contact(View):
 	def get(self, request, *args, **kwargs):
-		
 		template = "contact.html"
 		context = {}
 		return render(request, template, context)
@@ -48,22 +35,6 @@ class RootAPIView(APIView):
 				"posts": reverse('posts-api:list', request=request, format=format), 
 				"comments": reverse('comments-api:list', request=request, format=format),
 			})
-
-@api_view()
-def error400(request):
-	raise ParseError('Bad request')
-
-@api_view()
-def error403(request):
-	raise PermissionDenied('PermissionDenied')
-
-@api_view()
-def error404(request):
-	raise NotFound('Not found')
-
-@api_view()
-def error500(request):
-	raise APIException(detail='Server error')
 
 def translate(request):
 	return render(request, 'contact.html', {})
